d7da27c1fbc52aa52f2f74a6348f86277e4e5aa0
cyleleghorn <cyleleghorn@gmail.com>
2016-12-31 23:20:28 -0500

    Create GUI
    Created the GUI and added code for auto update.

---
+++ src/pcag/AutoUpdate.java
@@ -0,0 +1,180 @@
+package pcag;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+import org.apache.commons.io.FileUtils;
+
+public class AutoUpdate 
+{
+	private static String data=null;
+	private static String newVersion="0.0.0";
+	private static String[] currentVersion=MainGUI.version.split("\\.");
+	private static String[] remoteVersion = new String[]{"0","0","0"};
+
+	
+	public static void checkForUpdates(String callBackLocation)
+	{
+		Runnable r = new Runnable()
+		{
+			@Override
+			public void run() 
+			{
+				
+				URL url;
+				try {
+					url = new URL("http://islandpi.noip.me:8080/torrentclient/version.html");
+					InputStream html = null;
+
+			        html = url.openStream();
+			        
+			        int c = 0;
+			        StringBuffer buffer = new StringBuffer("");
+
+			        while(c != -1) {
+			            c = html.read();
+			            
+			        buffer.append((char)c);
+			        }
+			        
+			        data = buffer.toString();
+			        data = data.substring(data.indexOf("id=\"version\">")+13, data.indexOf("</div>"));
+			        remoteVersion = data.substring(1).split("\\.");
+				} 
+				catch (IOException e1) 
+				{
+					e1.printStackTrace();
+				}
+		        
+				//Make the decision on what to do.
+				if(!Arrays.equals(remoteVersion, new String[]{"0","0","0"}) && Arrays.equals(remoteVersion, MainGUI.version.split("\\.")))
+				{
+					if(callBackLocation.equals("LAUNCH"))
+					{
+						System.out.println("You have the latest version.");
+					}
+					else if(callBackLocation.equals("MENU"))
+					{
+						JOptionPane.showMessageDialog(MainGUI.mainFrame, "You have the latest version!", "Up to date", JOptionPane.INFORMATION_MESSAGE);
+					}
+				}
+				else if(!Arrays.equals(remoteVersion, new String[]{"0","0","0"}) && !Arrays.equals(remoteVersion, MainGUI.version.split("\\.")))
+				{
+					if(needsUpdate())  //Actually check to see if the remoteVersion is bigger.
+					{
+						System.out.println("You do not have the latest version.  Please update");
+						int updateOrNah = JOptionPane.showConfirmDialog(MainGUI.mainFrame, "There is a new version available! Do you wish to update?\nYour version: V" + MainGUI.version + "\nLatest version: V" + remoteVersion[0] + "." + remoteVersion[1] + "." + remoteVersion[2], "Please Update", JOptionPane.YES_NO_OPTION);
+						
+						if(updateOrNah==0)
+						{
+							//true; update
+							newVersion = remoteVersion[0] + "." + remoteVersion[1] + "." + remoteVersion[2]; 
+							update();
+						}
+						else
+						{
+							//They don't want to update. Fuck em.
+						}
+					}
+					else{System.out.println("Your version is higher than the server version.");}
+				}
+				else if(Arrays.equals(remoteVersion, new String[]{"0","0","0"}))
+				{
+					System.out.println("There was an error contacting the update server.");
+					JOptionPane.showMessageDialog(MainGUI.mainFrame, "Could not check for latest version!\nThe update server may be offline. Check your connection and try again.", "Update Error", JOptionPane.ERROR_MESSAGE);
+				}				
+			}
+		};
+		
+		new Thread(r).start();
+	}
+	
+	private static void update()
+	{
+		createBatch();
+		
+		//Download the new jar file
+		try {
+			FileUtils.copyURLToFile(new URL("http://islandpi.noip.me:8080/torrentclient/update.jar"), new File("update.jar"));
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(MainGUI.mainFrame, "Download failed!", "Error", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+		
+		//Run the batch file
+		
+		ProcessBuilder builder = new ProcessBuilder("cmd.exe","/C","start","update.bat");
+		try 
+		{
+			builder.start();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		//close it all out now.
+		System.exit(0);
+	}
+	
+	private static boolean needsUpdate()
+	{
+		for(int i=0; i<3; i++)
+		{
+			if(Integer.parseInt(remoteVersion[i]) > Integer.parseInt(currentVersion[i]))
+			{
+				return true;
+			}
+			else if(Integer.parseInt(remoteVersion[i]) < Integer.parseInt(currentVersion[i]))
+			{
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	private static void createBatch()
+	{
+		File batch = new File("update.bat");
+		try {
+			PrintWriter out = new PrintWriter(new FileOutputStream(batch));
+			
+			out.println("@ECHO off");
+			out.println("echo Updating, please wait...");
+			
+			//Get current file name
+			//Must use main class file.
+			File dir = new File(MainGUI.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+			String jar = System.getProperty("java.class.path");
+			File f = new File(dir, jar);
+			//JOptionPane.showMessageDialog(null, f.getName());
+			//System.out.println(f.getName());
+			String jarName = f.getName();
+			
+			
+			out.println("del \"" + jarName + "\"");
+			
+			out.println("rename update.jar \"Torrent Client V" + newVersion + ".jar\"");
+			
+			//This line deletes itself.
+			
+			out.println("start javaw -jar \"Torrent Client V" + newVersion + ".jar\"");
+			out.println("echo Done updating!  You can close this window now");
+			out.println("(goto) 2>nul & del \"%~f0\" & exit");
+			
+			
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- src/pcag/MainGUI.java
+++ src/pcag/MainGUI.java
@@ -1,15 +1,25 @@
 package pcag;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import net.miginfocom.swing.MigLayout;
 
 
 public class MainGUI
 {
 	
-	private JFrame frame;
+	public static JFrame	mainFrame;
+	public static String	version	= "V1.0.0";
 	
 	
 	/**
@@ -19,12 +29,13 @@ public class MainGUI
 	{
 		EventQueue.invokeLater(new Runnable()
 		{
+			@SuppressWarnings("static-access")
 			public void run()
 			{
 				try
 				{
 					MainGUI window = new MainGUI();
-					window.frame.setVisible(true);
+					window.mainFrame.setVisible(true);
 				}
 				catch (Exception e)
 				{
@@ -49,10 +60,72 @@ public class MainGUI
 	 */
 	private void initialize()
 	{
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new MigLayout("", "[]", "[]"));
+		// Set look and feel if not on windows.
+		if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+		{
+			try
+			{
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			}
+			catch (ClassNotFoundException e1)
+			{
+				e1.printStackTrace();
+			}
+			catch (InstantiationException e1)
+			{
+				e1.printStackTrace();
+			}
+			catch (IllegalAccessException e1)
+			{
+				e1.printStackTrace();
+			}
+			catch (UnsupportedLookAndFeelException e1)
+			{
+				e1.printStackTrace();
+			}
+		}
+		else
+		{
+			// Basically, if it is windows, do nothing and let it default to
+			// Java look and feel.
+		}
+		
+		mainFrame = new JFrame();
+		mainFrame.setBounds(100, 100, 500, 400);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.getContentPane().setLayout(new MigLayout("", "[grow][]", "[][grow]"));
+		
+		JButton btnGetData = new JButton("Get Data");
+		btnGetData.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		mainFrame.getContentPane().add(btnGetData, "cell 0 0,pushx ,growx,width 50%,height 10%");
+		
+		JButton btnExportData = new JButton("Export Data");
+		btnExportData.setEnabled(false);
+		btnExportData.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		mainFrame.getContentPane().add(btnExportData, "cell 1 0,pushx ,growx,width 50%,gapx 5%,height 10%");
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		textArea.setToolTipText("Status window");
+		mainFrame.getContentPane().add(textArea, "cell 0 1 2097051 1,push ,grow");
+		
+		JMenuBar menuBar = new JMenuBar();
+		mainFrame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmExportToExcel = new JMenuItem("Export to Excel");
+		mnFile.add(mntmExportToExcel);
+		
+		JMenu mnAbout = new JMenu("About");
+		menuBar.add(mnAbout);
+		
+		JMenuItem mntmCheckForUpdates = new JMenuItem("Check For Updates");
+		mnAbout.add(mntmCheckForUpdates);
+		
+		JMenuItem mntmHelp = new JMenuItem("Help");
+		mnAbout.add(mntmHelp);
 	}
 	
 }
